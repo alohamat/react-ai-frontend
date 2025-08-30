3fbdcf6e8ced20b43040b8c4699f2b21792d3d46
aloha <matalohazk@gmail.com>
2025-08-30 11:55:06 -0300

    working on responsive and dynamic layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,41 @@
 import Header from "./components/Header";
 import PromptArea from "./components/PromptArea";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [inChat, setInChat] = useState(false);
+  const [isBottom, setIsBottom] = useState(false);
 
-  useEffect(() => {
-    if (inChat) {
-      
-    }
-  }, [inChat]);
-
-  function handleSend() {
-    console.log("sent");
-    setInChat(true);
-  }
   return (
-    <div>
+    <div className="bg-gray-500 h-screen flex flex-col items-center justify-center relative">
       <Header />
-      <div className="bg-gray-500 h-screen flex justify-center items-center flex-col gap-4">
-        <h1 className="text-4xl">Talk to AI</h1>
-        <div className="flex justify-center items-center">
-          <PromptArea />
-          <button
-        className="bg-blue-500 hover:bg-blue-700 text-white p-3 ml-2 rounded-xl
-          transition-all ease-in-out duration-300 text-lg font-semibold"
-          onClick={(e) => {e.preventDefault(); handleSend();}}
-      >
-        Send
-      </button>
+      <h1 className="text-4xl mb-4">Talk to AI</h1>
+
+      {isBottom && (
+        <div className="absolute flex justify-center items-center z-10">
+          <div className="bg-white p-8 rounded-xl shadow-2xl text-black text-2xl text-center
+          w-[90vw] sm:w-[85vw] lg:w-[70vw] xl:w-[55vw]
+          ">
+            im the biggest bird
+          </div>
         </div>
+      )}
+
+      <div
+        className={`flex justify-center items-center transition-all duration-300 
+        ${isBottom ? "absolute bottom-4 w-full z-0" : ""}`}
+      >
+        <PromptArea />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white p-3 ml-2 rounded-xl
+          transition-all ease-in-out duration-300 text-lg font-semibold hover:cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault();
+            //handleSend();
+            setIsBottom(!isBottom);
+          }}
+        >
+          Send
+        </button>
       </div>
     </div>
   );
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,6 @@
 function Header() {
     return (
-        <header className="bg-gray-400 text-3xl flex justify-center">Chatbot</header>
+        <header className="bg-gray-400 text-3xl flex justify-center absolute top-0 w-screen">Chatbot</header>
     )
 }
 
